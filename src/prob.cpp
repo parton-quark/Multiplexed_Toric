0bcc34d50323c254680cb44e9ae6d6b695110ede
parton-quark <parton@nii.ac.jp>
2023-07-10 14:49:19 +0900

    add probabilistic_float

--- src/prob.cpp
+++ src/prob.cpp
@@ -14,3 +14,17 @@ bool probabilistic(float prob, std::mt19937& engine, std::uniform_real_distribut
     return result;
 }
 
+double probabilistic_float(std::mt19937& engine, std::uniform_real_distribution<double>& dist){
+    double val;
+    val = dist(engine);
+    return val;
+}
+
+
+weighted_random(std::vector<float> vec_weight, std::mt19937& engine, std::uniform_real_distribution<double>& dist){
+    int result;
+    val = dist(engine);
+    
+    //累積和に基づいて量子
+    
+}
--- src/prob.hpp
+++ src/prob.hpp
@@ -1,2 +1,3 @@
 bool probabilistic(float prob, std::mt19937& engine, std::uniform_real_distribution<double>& dist);
+double probabilistic_float(std::mt19937& engine, std::uniform_real_distribution<double>& dist);
 // int pick_random(std::vector<int>, std::mt19937& engine, std::uniform_real_distribution<double>& dist)

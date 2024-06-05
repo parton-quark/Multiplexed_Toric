b0c957ba513788b873affdb81e5cee948b9750ac
parton-quark <parton@nii.ac.jp>
2024-06-05 16:11:17 +0900

    add script files for compare m=1 and m = 4

--- src/make_parameters.py
+++ src/make_parameters.py
@@ -1,10 +1,11 @@
 # lattice_size, strategy, multiplexing, num_shots, force, error_type, p_burst
-lattice_size = [12]
-strategy = [4,5]
-multiplexing = [2,4]
+lattice_size = [8,10,12,14,16,18,20]
+strategy = [1]
+multiplexing = [4]
 num_shots = [100000]
-force = [0,1,2,4,8,16,32]
-p_burst = [0.1, 0.2, 0.4, 0.8, 1.0]
+force = [0]
+error_model = ["uniform"]
+p_burst = [0.1]
 
 texts = ""
 file = open('parameters.txt', 'w')
@@ -13,11 +14,14 @@ for l in lattice_size:
         for m in multiplexing:
             for shots in num_shots:
                 for f in force:
-                    for p in p_burst:
-                        text = str(l) + " " + str(s) + " " + str(m) + " " + str(shots) + " " + str(f) + " " + str(p) + '\n'
-                        texts = texts + text
+                    for e in error_model:
+                        for p in p_burst:
+                            text = str(l) + " " + str(s) + " " + str(m) + " " + str(shots) + " " + str(f) + " " + str(e) + " " + str(p) + '\n'
+                            texts = texts + text
 
 texts = texts.rstrip('\r\n')
 print(texts)
 file.write(texts)
 file.close()
+
+# lattice_size, strategy, multiplexing, num_shots, force, error_model, p_burst
